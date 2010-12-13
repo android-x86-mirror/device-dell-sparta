682c9d380370c862cc143b4a3cecaf9b9b941552
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-12-13 14:39:49 +0800

    initial commit for Dell Inspiron Mini Duo platform
    Contribution by Doug Anson <Ansondtx20@gmail.com> from Dell.

---
+++ sparta.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
+
+$(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
+
+PRODUCT_NAME := sparta
+PRODUCT_DEVICE := sparta
+PRODUCT_MANUFACTURER := dell
+PRODUCT_MODEL := Dell Mini Duo
+PRODUCT_BRAND := Dell
