cf65866142eaee7ff9ecc2f919e82f293753f286
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-01-24 00:25:53 +0800

    update for gingerbread-x86

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,12 +1,7 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := iwlwifi-6050-4.ucode LICENSE.iwlwifi-6050-ucode
-# LOCAL_KEYMAPS_DIR := $(LOCAL_PATH)/keymaps
+LOCAL_FIRMWARES := iwlwifi-6050-4.ucode LICENSE.iwlwifi
 # TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/sparta_info
-TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/sparta_defconfig
-
-# vold.fstab handling
-# PRODUCT_COPY_FILES += $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/eGestured:system/bin/eGestured
