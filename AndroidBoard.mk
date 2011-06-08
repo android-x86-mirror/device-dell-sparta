82139c02fd8ee923ebe569379527de3d0755eec3
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-06-08 17:02:39 +0800

    fix the missing license file

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := iwlwifi-6050-4.ucode LICENSE.iwlwifi-6050-ucode
+LOCAL_FIRMWARES := iwlwifi-6050-4.ucode LICENSE.iwlwifi
 # LOCAL_KEYMAPS_DIR := $(LOCAL_PATH)/keymaps
 # TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/sparta_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
