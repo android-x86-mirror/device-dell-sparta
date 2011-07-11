302f808d967f06d4d15a3b1f0998335257664957
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-07-11 09:42:59 +0800

    update kernel config and fix the touch issue

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 LOCAL_FIRMWARES := iwlwifi-6050-4.ucode LICENSE.iwlwifi
 # LOCAL_KEYMAPS_DIR := $(LOCAL_PATH)/keymaps
-# TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/sparta_info
+TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/sparta_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/sparta_defconfig
 
